ea833d77b6c0b1fff484b9b060255d22db62e8dc
Andres Villalobos <andres.villalobos@datagrove.com>
2022-10-11 17:49:22 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,16 @@ import './index.css'
 //     <App />
 //   </React.StrictMode>
 // )
+//new imports
+import "prosemirror-find-replace"
+//end of new imports
 import { EditorState } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
 import { Schema, DOMParser, DOMSerializer } from "prosemirror-model"
 import { schema } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
 import { exampleSetup } from "prosemirror-example-setup"
+
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
@@ -39,5 +43,20 @@ document.getElementById('go')?.addEventListener('click', () => {
     doc: DOMParser.fromSchema(mySchema).parse(content),
     plugins: exampleSetup({ schema: mySchema })
   }))
+})
+
+let pm = new ProseMirror({
+  place: document.querySelector("#target"),
+  find: {}
+})
+
+defineOption("find", false, (pm, value) => {
+  if (pm.mod.find) {
+    pm.mod.find.detach()
+    pm.mod.find = null
+  }
+  if (value) {
+    pm.mod.find = new Find(pm, value)
+  }
 })
 
