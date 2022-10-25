e0416464de6e15299608a38a5731af89aa9a3059
Andres Villalobos <andres.villalobos@datagrove.com>
2022-10-25 08:24:43 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -88,190 +88,134 @@ document.getElementById('go2')?.addEventListener('click', () => {
 // }
 
 //linter code
-import {schema as baseSchema} from "prosemirror-schema-basic"
-import {Schema} from "prosemirror-model"
+// lint{
+// Words you probably shouldn't use
+const badWords = /\b(obviously|clearly|evidently|simply)\b/ig
+// Matches punctuation with a space before it
+const badPunc = / ([,\.!?:]) ?/g
 
-let baseNodes = baseSchema.spec.nodes
-const schema = new Schema({
-  nodes: baseNodes.update("code_block", Object.assign(
-    {}, baseNodes.get("code_block"), {isolating: true})),
-  marks: baseSchema.spec.marks
-})
-// }
-// nodeview_start{
-import {
-  EditorView as CodeMirror, keymap as cmKeymap, drawSelection
-} from "@codemirror/view"
-import {javascript} from "@codemirror/lang-javascript"
-import {defaultKeymap} from "@codemirror/commands"
-import {syntaxHighlighting, defaultHighlightStyle} from "@codemirror/language"
+function lint(document: Document) {
+  let result = [], lastHeadLevel = null
 
-import {exitCode} from "prosemirror-commands"
-import {undo, redo} from "prosemirror-history"
-
-class CodeBlockView {
-  constructor(node, view, getPos) {
-    // Store for later
-    this.node = node
-    this.view = view
-    this.getPos = getPos
-
-    // Create a CodeMirror instance
-    this.cm = new CodeMirror({
-      doc: this.node.textContent,
-      extensions: [
-        cmKeymap.of([
-          ...this.codeMirrorKeymap(),
-          ...defaultKeymap
-        ]),
-        drawSelection(),
-        syntaxHighlighting(defaultHighlightStyle),
-        javascript(),
-        CodeMirror.updateListener.of(update => this.forwardUpdate(update))
-      ]
-    })
-
-    // The editor's outer node is our DOM representation
-    this.dom = this.cm.dom
-
-    // This flag is used to avoid an update loop between the outer and
-    // inner editor
-    this.updating = false
+  function record(msg, from, to, fix) {
+    result.push({msg, from, to, fix})
   }
-// }
-// nodeview_forwardUpdate{
-  forwardUpdate(update) {
-    if (this.updating || !this.cm.hasFocus) return
-    let offset = this.getPos() + 1, {main} = update.state.selection
-    let selFrom = offset + main.from, selTo = offset + main.to
-    let pmSel = this.view.state.selection
-    if (update.docChanged || pmSel.from != selFrom || pmSel.to != selTo) {
-      let tr = this.view.state.tr
-      update.changes.iterChanges((fromA, toA, fromB, toB, text) => {
-        if (text.length)
-          tr.replaceWith(offset + fromA, offset + toA,
-                         schema.text(text.toString()))
-        else
-          tr.delete(offset + fromA, offset + toA)
-        offset += (toB - fromB) - (toA - fromA)
-      })
-      tr.setSelection(TextSelection.create(tr.doc, selFrom, selTo))
-      this.view.dispatch(tr)
+
+  // For each node in the document
+  doc.descendants((node, pos) => {
+    if (node.isText) {
+      // Scan text nodes for suspicious patterns
+      let m
+      while (m = badWords.exec(node.text))
+        record(`Try not to say '${m[0]}'`,
+               pos + m.index, pos + m.index + m[0].length)
+      while (m = badPunc.exec(node.text))
+        record("Suspicious spacing around punctuation",
+               pos + m.index, pos + m.index + m[0].length,
+               fixPunc(m[1] + " "))
+    } else if (node.type.name == "heading") {
+      // Check whether heading levels fit under the current level
+      let level = node.attrs.level
+      if (lastHeadLevel != null && level > lastHeadLevel + 1)
+        record(`Heading too small (${level} under ${lastHeadLevel})`,
+               pos + 1, pos + 1 + node.content.size,
+               fixHeader(lastHeadLevel + 1))
+      lastHeadLevel = level
+    } else if (node.type.name == "image" && !node.attrs.alt) {
+      // Ensure images have alt text
+      record("Image without alt text", pos, pos + 1, addAlt)
     }
-  }
-// }
-// nodeview_setSelection{
-  setSelection(anchor, head) {
-    this.cm.focus()
-    this.updating = true
-    this.cm.dispatch({selection: {anchor, head}})
-    this.updating = false
-  }
-// }
-// nodeview_keymap{
-  codeMirrorKeymap() {
-    let view = this.view
-    return [
-      {key: "ArrowUp", run: () => this.maybeEscape("line", -1)},
-      {key: "ArrowLeft", run: () => this.maybeEscape("char", -1)},
-      {key: "ArrowDown", run: () => this.maybeEscape("line", 1)},
-      {key: "ArrowRight", run: () => this.maybeEscape("char", 1)},
-      {key: "Ctrl-Enter", run: () => {
-        if (!exitCode(view.state, view.dispatch)) return false
-        view.focus()
-        return true
-      }},
-      {key: "Ctrl-z", mac: "Cmd-z",
-       run: () => undo(view.state, view.dispatch)},
-      {key: "Shift-Ctrl-z", mac: "Shift-Cmd-z",
-       run: () => redo(view.state, view.dispatch)},
-      {key: "Ctrl-y", mac: "Cmd-y",
-       run: () => redo(view.state, view.dispatch)}
-    ]
-  }
+  })
 
-  maybeEscape(unit, dir) {
-    let {state} = this.cm, {main} = state.selection
-    if (!main.empty) return false
-    if (unit == "line") main = state.doc.lineAt(main.head)
-    if (dir < 0 ? main.from > 0 : main.to < state.doc.length) return false
-    let targetPos = this.getPos() + (dir < 0 ? 0 : this.node.nodeSize)
-    let selection = Selection.near(this.view.state.doc.resolve(targetPos), dir)
-    let tr = this.view.state.tr.setSelection(selection).scrollIntoView()
-    this.view.dispatch(tr)
-    this.view.focus()
-  }
+  return result
+}
 // }
-// nodeview_update{
-  update(node) {
-    if (node.type != this.node.type) return false
-    this.node = node
-    if (this.updating) return true
-    let newText = node.textContent, curText = this.cm.state.doc.toString()
-    if (newText != curText) {
-      let start = 0, curEnd = curText.length, newEnd = newText.length
-      while (start < curEnd &&
-             curText.charCodeAt(start) == newText.charCodeAt(start)) {
-        ++start
+
+// fix{
+function fixPunc(replacement) {
+  return function({state, dispatch}) {
+    dispatch(state.tr.replaceWith(this.from, this.to,
+                                  state.schema.text(replacement)))
+  }
+}
+
+function fixHeader(level) {
+  return function({state, dispatch}) {
+    dispatch(state.tr.setNodeMarkup(this.from - 1, null, {level}))
+  }
+}
+
+function addAlt({state, dispatch}) {
+  let alt = prompt("Alt text", "")
+  if (alt) {
+    let attrs = Object.assign({}, state.doc.nodeAt(this.from).attrs, {alt})
+    dispatch(state.tr.setNodeMarkup(this.from, null, attrs))
+  }
+}
+// }
+
+// deco{
+import {Decoration, DecorationSet} from "prosemirror-view"
+
+function lintDeco(doc) {
+  let decos = []
+  lint(doc).forEach(prob => {
+    decos.push(Decoration.inline(prob.from, prob.to, {class: "problem"}),
+               Decoration.widget(prob.from, lintIcon(prob)))
+  })
+  return DecorationSet.create(doc, decos)
+}
+
+function lintIcon(prob) {
+  let icon = document.createElement("div")
+  icon.className = "lint-icon"
+  icon.title = prob.msg
+  icon.problem = prob
+  return icon
+}
+// }
+
+// plugin{
+import {Plugin, TextSelection} from "prosemirror-state"
+
+let lintPlugin = new Plugin({
+  state: {
+    init(_, {doc}) { return lintDeco(doc) },
+    apply(tr, old) { return tr.docChanged ? lintDeco(tr.doc) : old }
+  },
+  props: {
+    decorations(state) { return this.getState(state) },
+    handleClick(view, _, event) {
+      if (/lint-icon/.test(event.target.className)) {
+        let {from, to} = event.target.problem
+        view.dispatch(
+          view.state.tr
+            .setSelection(TextSelection.create(view.state.doc, from, to))
+            .scrollIntoView())
+        return true
       }
-      while (curEnd > start && newEnd > start &&
-             curText.charCodeAt(curEnd - 1) == newText.charCodeAt(newEnd - 1)) {
-        curEnd--
-        newEnd--
-      }
-      this.updating = true
-      this.cm.dispatch({
-        changes: {
-          from: start, to: curEnd,
-          insert: newText.slice(start, newEnd)
+    },
+    handleDoubleClick(view, _, event) {
+      if (/lint-icon/.test(event.target.className)) {
+        let prob = event.target.problem
+        if (prob.fix) {
+          prob.fix(view)
+          view.focus()
+          return true
         }
-      })
-      this.updating = false
-    }
-    return true
-  }
-// }
-// nodeview_end{
-
-  selectNode() { this.cm.focus() }
-  stopEvent() { return true }
-}
-// }
-// arrowHandlers{
-import {keymap} from "prosemirror-keymap"
-
-function arrowHandler(dir) {
-  return (state, dispatch, view) => {
-    if (state.selection.empty && view.endOfTextblock(dir)) {
-      let side = dir == "left" || dir == "up" ? -1 : 1
-      let $head = state.selection.$head
-      let nextPos = Selection.near(
-        state.doc.resolve(side > 0 ? $head.after() : $head.before()), side)
-      if (nextPos.$head && nextPos.$head.parent.type.name == "code_block") {
-        dispatch(state.tr.setSelection(nextPos))
-        return true
       }
     }
-    return false
   }
-}
-
-const arrowHandlers = keymap({
-  ArrowLeft: arrowHandler("left"),
-  ArrowRight: arrowHandler("right"),
-  ArrowUp: arrowHandler("up"),
-  ArrowDown: arrowHandler("down")
 })
 // }
 
 // editor{
 
 
-window.view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(schema).parse(document.querySelector("#content")),
-    plugins: exampleSetup({schema}).concat(arrowHandlers)
-  }),
-  nodeViews: {code_block: (node, view, getPos) => new CodeBlockView(node, view, getPos)}
+let state = EditorState.create({
+  doc: DOMParser.fromSchema(schema).parse(document.querySelector("#content")),
+  plugins: exampleSetup({schema}).concat(lintPlugin)
 })
+
+window.view = new EditorView(document.querySelector("#editor"), {state})
 // }
