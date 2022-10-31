a553873f455c6eb253c816412d17d6446b2e6f02
Andres Villalobos <andres.villalobos@datagrove.com>
2022-10-31 13:11:06 -0600

    Testing highlight added find to function
    Added the new find inside a function to see if I can get it to highlight

--- src/main.tsx
+++ src/main.tsx
@@ -2,36 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import "prosemirror-image-plugin/dist/styles/common.css";
+import "prosemirror-image-plugin/dist/styles/withResize.css";
+import "prosemirror-image-plugin/dist/styles/sideResize.css";
+import { lintDeco } from './lint';
 
 // ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
 // )
-
-import { EditorState } from "prosemirror-state"
+import { EditorState, Transaction } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
-import { Schema, DOMParser, DOMSerializer, Fragment, Node, NodeType  } from "prosemirror-model"
-import { schema } from "prosemirror-schema-basic"
-import { addListNodes } from "prosemirror-schema-list"
+import { DOMParser } from "prosemirror-model"
 import { exampleSetup } from "prosemirror-example-setup"
+import { mySchema,initialDoc } from "./schema"
+import {CodeBlockView,arrowHandlers} from "./codemirror"
+import { defaultSettings, updateImageNode, imagePlugin } from "prosemirror-image-plugin"
 
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-  marks: schema.spec.marks
-});
 let editor = document.querySelector("#editor")!
 let content = document.querySelector("#content")!
 let view = new EditorView(editor, {
   state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(content),
-    plugins: exampleSetup({ schema: mySchema })
-  })
+    doc: mySchema.nodeFromJSON(initialDoc),
+    plugins: [
+      ...exampleSetup({ schema: mySchema }).concat(arrowHandlers),
+      imagePlugin(mySchema, { ...defaultSettings }),
+    ]
+  }),
+  nodeViews: {code_block: (node, view, getPos) => new CodeBlockView(node, view, getPos)}
 })
-//Replace
+
+
 let search = document.querySelector('#search') as HTMLInputElement;
 let replace = document.querySelector('#replace') as HTMLInputElement;
 document.getElementById('go')?.addEventListener('click', () => {
@@ -58,15 +61,17 @@ document.getElementById('go2')?.addEventListener('click', () => {
 })
 */
 
-//new find to test
-let find = document.querySelector('#find') as HTMLInputElement;
+//new find 
+function newFind (doc: Node) {
+  let find = document.querySelector('#find') as HTMLInputElement;
 let nuevo = "";
 document.getElementById('go2')?.addEventListener('click', () => {
   let s = editor.querySelector(".ProseMirror")!.innerHTML
   if(s.includes(find.value)){
     nuevo = find.value
     //nuevo = nuevo.bold() // bold
-    nuevo = nuevo.toUpperCase() //sets found word to upper case 
+    //nuevo = nuevo.toUpperCase() //sets found word to upper case 
+    lintDeco(nuevo)
   }
   content.innerHTML = s.replaceAll(find.value, nuevo)
   view.updateState(EditorState.create({
@@ -74,6 +79,7 @@ document.getElementById('go2')?.addEventListener('click', () => {
     plugins: exampleSetup({ schema: mySchema })
   }))
 })
+}
 
 // editor{
 // let state = EditorState.create({
